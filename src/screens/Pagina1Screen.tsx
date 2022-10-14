47136825b2420dc75b3ef31a96800850c1662d77
StackSolutions <stacksolutionscl@gmail.com>
2022-10-14 12:36:21 -0300

    first commit

---
+++ src/screens/Pagina1Screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const Pagina1Screen = () => {
+    return (
+        <View><Text>Pagina1Screen</Text></View>
+    )
+}
+
+export default Pagina1Screen
---
+++ src/screens/Pagina2Screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const Pagina2Screen = () => {
+    return (
+        <View><Text>Pagina2Screen</Text></View>
+    )
+}
+
+export default Pagina2Screen
